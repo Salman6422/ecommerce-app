a4ba40ef8df88cda3ba24787138e59f13cedd75c
Salman Ahmed <151397371+Salman6422@users.noreply.github.com>
2025-07-17 10:17:18 +0500

    committing 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
     {/* hero left side */}
     <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
       <div className='text-[#414141]'>
+
         <div className='flex items-center gap-2'>
           <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
           <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
@@ -23,6 +24,7 @@ const Hero = () => {
     </div>
 
     {/* hero right side */}
+    
     <img src={assets.hero_img} className='w-full sm:w-1/2 ' alt="" />
 
   </div>
